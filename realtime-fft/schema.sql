637cf302cf41796fe8f50931330ea50ce1087528
sjkim <andrew.kim@machbase.com>
2024-05-08 15:08:58 +0900

    add retention

--- realtime-fft/schema.sql
+++ realtime-fft/schema.sql
@@ -6,6 +6,6 @@ select count(*) from fft;
 
 delete from fft;
 
-
-
-
+CREATE RETENTION policy_1m_1d DURATION 1 MONTH INTERVAL 1 DAY;
+ALTER TABLE fft ADD RETENTION policy_1m_1d;
+SELECT * FROM V$RETENTION_JOB;
