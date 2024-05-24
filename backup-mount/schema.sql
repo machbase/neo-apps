cda5f97ea0984dc29de30660a0a82a304a545c39
sjkim <andrew.kim@machbase.com>
2024-05-24 14:40:11 +0900

    add mount

--- backup-mount/schema.sql
+++ backup-mount/schema.sql
@@ -1,20 +1,71 @@
-drop table mytab cascade;
+drop table mytab_A cascade;
+drop table mytab_B cascade;
 
-create tag table if not exists mytab (name varchar(32) primary key, time datetime basetime, value double summarized);
+create tag table if not exists mytab_A (name varchar(32) primary key, time datetime basetime, value double summarized) tag_partition_count=1;
+create tag table if not exists mytab_B (name varchar(32) primary key, time datetime basetime, value double summarized) tag_partition_count=1;;
 
-delete from mytab;
+delete from mytab_A;
+delete from mytab_B;
 
-select * from v$mytab_stat;
+-- online data check
+select min(time), max(time) from mytab_A;
+select count(*) from mytab_A;
+select * from mytab_A limit 10;
 
-select * from mytab limit 100;
+select min(time), max(time) from mytab_B;
+select count(*) from mytab_B;
+select * from mytab_B limit 10;
 
-backup table mytab into disk='/tmp/mytab1';
+-- 1. backup full database 
 
-BACKUP TABLE mytab FROM TO_DATE('2023-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS')
-                         TO TO_DATE('2023-01-31 23:59:59','YYYY-MM-DD HH24:MI:SS')
-                         INTO DISK = '/tmp/mytab-time1';
+backup database  into disk='/tmp/mybkup1';
+
+mount database '/tmp/mybkup1' to mountdb1;
+
+select min(time), max(time) from mountdb1.sys.mytab_A;
+select count(*) from mountdb1.sys.mytab_A;
+select * from mountdb1.sys.mytab_A limit 10;
 
 
-MOUNT database '/tmp/mytab1' to mnt1;
+select min(time), max(time) from mountdb1.sys.mytab_B;
+select count(*) from mountdb1.sys.mytab_B;
+select * from mountdb1.sys.mytab_B limit 10;
 
-MOUNT database '/tmp/mytab-time1' to mnt2;
+umount database mountdb1;
+
+-- 2. backup full database within time-range
+
+backup database  FROM TO_DATE('2023-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS')
+                         TO TO_DATE('2023-01-31 23:59:59','YYYY-MM-DD HH24:MI:SS') into disk='/tmp/mybkup2';
+
+delete from mytab_B before TO_DATE('2023-01-31 23:59:59','YYYY-MM-DD HH24:MI:SS');
+
+select min(time), max(time) from mytab_B;
+
+select * from mytab_b where time between TO_DATE('2023-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS')
+                         and TO_DATE('2023-01-31 23:59:59','YYYY-MM-DD HH24:MI:SS');
+
+mount database '/tmp/mybkup2' to mountdb2;
+
+select min(time), max(time) from mountdb2.sys.mytab_B;
+select count(*) from mountdb2.sys.mytab_B;
+select * from mountdb2.sys.mytab_B limit 10;
+
+umount database mountdb2;
+
+-- 3. backup table within time-range
+
+BACKUP TABLE mytab_A FROM TO_DATE('2023-03-01 00:00:00','YYYY-MM-DD HH24:MI:SS')
+                         TO TO_DATE('2023-03-31 23:59:59','YYYY-MM-DD HH24:MI:SS')
+                         INTO DISK = '/tmp/mybkup3';
+
+MOUNT database '/tmp/mybkup3' to mountdb3;
+
+select min(time), max(time) from mountdb3.sys.mytab_A;
+select count(*) from mountdb3.sys.mytab_A;
+select * from mountdb3.sys.mytab_A limit 10;
+
+umount database mountdb3;
+
+
+
